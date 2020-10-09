69ac9fe0e65f300f0e761891d5b488b0e1e726ca
Krilian <tank2114@gmail.com>
2020-10-09 20:08:40 +0200

    Exercise3 Done

---
+++ SPDVIWExercise3/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SPDVIWExercise3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Value1 = new System.Windows.Forms.TextBox();
+            this.Value2 = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SumButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Value1
+            // 
+            this.Value1.Location = new System.Drawing.Point(199, 29);
+            this.Value1.Name = "Value1";
+            this.Value1.Size = new System.Drawing.Size(181, 20);
+            this.Value1.TabIndex = 0;
+            this.Value1.Text = "0";
+            // 
+            // Value2
+            // 
+            this.Value2.Location = new System.Drawing.Point(199, 76);
+            this.Value2.Name = "Value2";
+            this.Value2.Size = new System.Drawing.Size(181, 20);
+            this.Value2.TabIndex = 1;
+            this.Value2.Text = "0";
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(199, 151);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(181, 20);
+            this.Result.TabIndex = 2;
+            this.Result.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "First Value";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Second Value";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result";
+            // 
+            // SumButton
+            // 
+            this.SumButton.Location = new System.Drawing.Point(199, 118);
+            this.SumButton.Name = "SumButton";
+            this.SumButton.Size = new System.Drawing.Size(75, 23);
+            this.SumButton.TabIndex = 6;
+            this.SumButton.Text = "+";
+            this.SumButton.UseVisualStyleBackColor = true;
+            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(305, 118);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 7;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 197);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.SumButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Value2);
+            this.Controls.Add(this.Value1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Value1;
+        private System.Windows.Forms.TextBox Value2;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SumButton;
+        private System.Windows.Forms.Button ResetButton;
+    }
+}
+
---
+++ SPDVIWExercise3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPDVIWExercise3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SumButton_Click(object sender, EventArgs e)
+        {
+            Result.Text = (Convert.ToInt32(Value1.Text) + Convert.ToInt32(Value2.Text)).ToString();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            Value1.Text = 0.ToString();
+            Value2.Text = 0.ToString();
+            Result.Text = 0.ToString();
+        }
+    }
+}
