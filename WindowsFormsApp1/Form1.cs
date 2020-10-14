52e152a09f592d264fa33721c100b961e02c2770
Krilian <tank2114@gmail.com>
2020-10-14 19:42:03 +0200

    Exercise4a Re-Done and Exercise4b started

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,6 +12,8 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private int value = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +21,12 @@ namespace WindowsFormsApp1
 
         private void SumButton_Click(object sender, EventArgs e)
         {
-            Result.Text = (Convert.ToInt32(Value1.Text) + Convert.ToInt32(Value2.Text)).ToString();
+            value = 1;
         }
 
         private void SubButton_Click(object sender, EventArgs e)
         {
-            Result.Text = (Convert.ToInt32(Value1.Text) - Convert.ToInt32(Value2.Text)).ToString();
+            value = 2;
         }
 
         private void ResetButton_Click(object sender, EventArgs e)
@@ -32,6 +34,24 @@ namespace WindowsFormsApp1
             Value1.Text = 0.ToString();
             Value2.Text = 0.ToString();
             Result.Text = 0.ToString();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            if(value == 1)
+            {
+                Result.Text = (Convert.ToInt32(Value1.Text) + Convert.ToInt32(Value2.Text)).ToString();
+                value = 0;
+            }
+            else if(value == 2)
+            {
+                Result.Text = (Convert.ToInt32(Value1.Text) - Convert.ToInt32(Value2.Text)).ToString();
+                value = 0;
+            }
+            else
+            {
+                MessageBox.Show("No Operation selected");
+            }
         }
     }
 }
--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -31,11 +31,9 @@
             this.Value1 = new System.Windows.Forms.TextBox();
             this.Value2 = new System.Windows.Forms.TextBox();
             this.Result = new System.Windows.Forms.TextBox();
-            this.SumButton = new System.Windows.Forms.Button();
             this.ResetButton = new System.Windows.Forms.Button();
-            this.SubButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.CalculateButton = new System.Windows.Forms.Button();
+            this.Options = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // Value1
@@ -62,16 +60,6 @@
             this.Result.TabIndex = 2;
             this.Result.Text = "0";
             // 
-            // SumButton
-            // 
-            this.SumButton.Location = new System.Drawing.Point(248, 84);
-            this.SumButton.Name = "SumButton";
-            this.SumButton.Size = new System.Drawing.Size(75, 23);
-            this.SumButton.TabIndex = 3;
-            this.SumButton.Text = "+";
-            this.SumButton.UseVisualStyleBackColor = true;
-            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
-            // 
             // ResetButton
             // 
             this.ResetButton.Location = new System.Drawing.Point(248, 176);
@@ -82,16 +70,6 @@
             this.ResetButton.UseVisualStyleBackColor = true;
             this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
             // 
-            // SubButton
-            // 
-            this.SubButton.Location = new System.Drawing.Point(248, 130);
-            this.SubButton.Name = "SubButton";
-            this.SubButton.Size = new System.Drawing.Size(75, 23);
-            this.SubButton.TabIndex = 5;
-            this.SubButton.Text = "-";
-            this.SubButton.UseVisualStyleBackColor = true;
-            this.SubButton.Click += new System.EventHandler(this.SubButton_Click);
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -101,26 +79,25 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "Select a Operation";
             // 
-            // CalculateButton
+            // Options
             // 
-            this.CalculateButton.Location = new System.Drawing.Point(101, 176);
-            this.CalculateButton.Name = "CalculateButton";
-            this.CalculateButton.Size = new System.Drawing.Size(75, 23);
-            this.CalculateButton.TabIndex = 7;
-            this.CalculateButton.Text = "Calculate";
-            this.CalculateButton.UseVisualStyleBackColor = true;
-            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            this.Options.FormattingEnabled = true;
+            this.Options.Items.AddRange(new object[] {
+            "+",
+            "-"});
+            this.Options.Location = new System.Drawing.Point(230, 75);
+            this.Options.Name = "Options";
+            this.Options.Size = new System.Drawing.Size(120, 17);
+            this.Options.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(559, 259);
-            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.Options);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.SubButton);
             this.Controls.Add(this.ResetButton);
-            this.Controls.Add(this.SumButton);
             this.Controls.Add(this.Result);
             this.Controls.Add(this.Value2);
             this.Controls.Add(this.Value1);
@@ -136,11 +113,9 @@
         private System.Windows.Forms.TextBox Value1;
         private System.Windows.Forms.TextBox Value2;
         private System.Windows.Forms.TextBox Result;
-        private System.Windows.Forms.Button SumButton;
         private System.Windows.Forms.Button ResetButton;
-        private System.Windows.Forms.Button SubButton;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.ListBox Options;
     }
 }
 
